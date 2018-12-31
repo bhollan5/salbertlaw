5433651da5461e54d6b283386bf27a2ff8de1fa6
Ben Holland <bhollan5@kent.edu>
2018-12-30 20:39:33 -0500

    Initial framing of website

---
+++ src/views/HeaderFrame.vue
@@ -0,0 +1,70 @@
+<template>
+  <div id="header-frame">
+    <div id="logo">
+      <img src="@/assets/albert_logo.svg">
+    </div>
+    <div id="menu-options">
+    
+    </div>
+    <div id="image-banner">
+    
+    </div>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: 'home',
+  components: {
+  }
+}
+</script>
+
+<style scoped lang="scss">
+@import "~@/GlobalVars.scss";
+
+#header-frame {
+  position: absolute;
+  display: grid;
+  grid-template-rows: 100px 30px 300px 1fr;
+  grid-template-columns: 10% 80% 10%;
+  grid-template-areas: 
+    "margin-top margin-top margin-top"
+    "menu-options menu-options menu-options"
+    "banner-image banner-image banner-image"
+    "margin-left content margin-right";
+  left: 0;
+  top: 0;
+  width: 100%;
+  min-height: 100%;
+}
+#menu-options {
+  background: $light-gray;
+  box-shadow: $inset-box-shading;
+  grid-row: 2 / 3;
+  grid-column: 1 / 4;
+  width: 90%;
+  padding: 0% 10%;
+  height: 30px;
+  margin-left: -5%;
+}
+
+#logo {
+  grid-column: 2/3;
+  grid-row: 1/4;
+  width: 300px;
+  height: 140px;
+  margin-left: 50px;
+  background: white;
+  z-index: 100;
+  padding: 25px;
+  box-shadow: $box-shading;
+}
+#image-banner {
+  background: $dark-gray;
+  grid-area: banner-image;
+  box-shadow: $heavy-inset-box-shading;
+}
+</style>
